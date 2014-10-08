6474fdee3817bdbc60c4c98ed291374749b59362
ekoc86 <okey@Fabians-iMac.local>
2014-10-08 11:26:35 -0400

    ekle

---
+++ rubysteps.git.rb
@@ -0,0 +1,10 @@
+touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/ekoc86/Ruby_Steps.git
+git push -u origin master
+
+git clone https://github.com/RubySteps/rubysteps.git
+cd rubysteps
+vagrant up
